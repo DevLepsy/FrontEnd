2630d53213ac8d333a651c7a110860e386fe6a16
AdLepsy <henryck.paris@viacesi.fr>
2023-11-03 10:31:36 +0100

    Update login/logout functionality in auth.js

--- assets/src/auth.js
+++ assets/src/auth.js
@@ -1,19 +1,27 @@
 export function updateNavBasedOnAuthState() {
   const loginLink = document.getElementById("loginLink");
-  if (sessionStorage.getItem("accessToken")) {
-    loginLink.textContent = "Logout";
-    loginLink.href = "#";
+
+  if (loginLink) {
+    if (sessionStorage.getItem("accessToken")) {
+      loginLink.textContent = "logout";
+      loginLink.href = "#";
+    } else {
+      loginLink.textContent = "login";
+      loginLink.href = "./login.html";
+    }
   } else {
-    loginLink.textContent = "Login";
-    loginLink.href = "./login.html";
+    console.warn("Element with ID 'loginLink' not found in the DOM.");
   }
 }
 
 export function handleLoginClick(event) {
-  if (event.target.textContent === "Logout") {
+  if (event.target.textContent === "logout") {
     event.preventDefault();
     sessionStorage.removeItem("accessToken");
     updateNavBasedOnAuthState();
-    window.location.href = "./index.html";
+    window.location.replace("./index.html");
+  } else if (event.target.textContent === "login") {
+    // Ici, vous pouvez ajouter toute logique supplémentaire pour la connexion si nécessaire.
+    window.location.replace("./index.html");
   }
 }
