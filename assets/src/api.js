7a754fea2cd59b628f837f5f1688bb2aaeaffaa5
AdLepsy <henryck.paris@viacesi.fr>
2023-11-03 10:31:30 +0100

    Refactor API requests and add error handling

--- assets/src/api.js
+++ assets/src/api.js
@@ -1,46 +1,56 @@
-const API_CONFIG = {
-  BASE_URL: "http://localhost:5678/api",
-  ENDPOINTS: {
-    WORKS: "/works",
-    CATEGORIES: "/categories",
-    LOGIN: "/users/login",
-  },
-};
+import { API_CONFIG } from "./variables.js";
+
+async function makeRequest(
+  endpoint,
+  method = "GET",
+  bodyData = null,
+  customHeaders = {}
+) {
+  const headers = { "Content-Type": "application/json", ...customHeaders };
+
+  const token = sessionStorage.getItem("accessToken");
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  const body = bodyData ? JSON.stringify(bodyData) : null;
+
+  const response = await fetch(API_CONFIG.BASE_URL + endpoint, {
+    method,
+    headers,
+    body,
+  });
+
+  if (!response.ok) {
+    let errorMsg = `Erreur ${response.status}: ${response.statusText}`;
+    try {
+      const responseData = await response.json();
+      if (responseData && responseData.error) {
+        errorMsg = responseData.error;
+      }
+    } catch (error) {}
+
+    throw {
+      status: response.status,
+      statusText: response.statusText,
+      message: errorMsg,
+      body: await response.json(),
+    };
+  }
+
+  return response.json();
+}
 
 async function fetchProjects() {
-  const response = await fetch(
-    API_CONFIG.BASE_URL + API_CONFIG.ENDPOINTS.WORKS
-  );
-  if (!response.ok) {
-    throw new Error(`Erreur ${response.status}: ${response.statusText}`);
-  }
-  return response.json();
+  return makeRequest(API_CONFIG.ENDPOINTS.WORKS);
 }
 
 async function fetchCategories() {
-  const response = await fetch(
-    API_CONFIG.BASE_URL + API_CONFIG.ENDPOINTS.CATEGORIES
-  );
-  if (!response.ok) {
-    throw new Error(`Erreur ${response.status}: ${response.statusText}`);
-  }
-  return response.json();
+  return makeRequest(API_CONFIG.ENDPOINTS.CATEGORIES);
 }
 
 async function login(email, password) {
-  const response = await fetch(
-    API_CONFIG.BASE_URL + API_CONFIG.ENDPOINTS.LOGIN,
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    }
-  );
-
-  if (!response.ok) {
-    return "failure";
-  }
-  return response.json();
+  return makeRequest(API_CONFIG.ENDPOINTS.LOGIN, "POST", { email, password });
 }
 
 export { fetchProjects, fetchCategories, login };
