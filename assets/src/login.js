7b64dc9c7c28791fac3cbcba4f22cef0233efa7a
AdLepsy <henryck.paris@viacesi.fr>
2023-11-03 10:32:11 +0100

    Refactor login form submission handling.

--- assets/src/login.js
+++ assets/src/login.js
@@ -2,36 +2,37 @@ import { updateNavBasedOnAuthState, handleLoginClick } from "./auth.js";
 import { login } from "./api.js";
 
 const form = document.querySelector("form");
-
-// Ajoute un gestionnaire d'événements pour le lien de connexion/logout
 const loginLinkElement = document.getElementById("loginLink");
+const connexion = document.querySelector("div");
 
 if (loginLinkElement) {
   loginLinkElement.addEventListener("click", handleLoginClick);
 }
 
 form.addEventListener("submit", async (e) => {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
   e.preventDefault();
+
+  const { email, password } = e.target.elements; // Destructuration
+
   try {
-    const response = await login(email, password);
-    if (response === "failure") {
-      const connexion = document.querySelector("div");
-      const error = document.createElement("p");
-      error.innerText = `"Erreur dans l’identifiant ou le mot de passe"`;
-      error.style.textAlign = "center";
-      error.style.color = "red";
-      error.style.marginBottom = "15px";
-      connexion.insertBefore(error, connexion.lastElementChild);
-    } else {
-      sessionStorage.setItem("accessToken", response.token);
-      window.location.href = "./index.html";
-    }
+    const response = await login(email.value, password.value);
+    sessionStorage.setItem("accessToken", response.token);
+    window.location.href = "./index.html";
   } catch (error) {
-    console.log(error);
+    // Suppression du message d'erreur précédent s'il existe
+    const existingError = document.querySelector(".error-message");
+    if (existingError) {
+      existingError.remove();
+    }
+
+    const errorMessage = document.createElement("p");
+    errorMessage.innerText = "Erreur dans l’identifiant ou le mot de passe";
+    errorMessage.classList.add("error-message");
+    errorMessage.style.textAlign = "center";
+    errorMessage.style.color = "red";
+    errorMessage.style.marginBottom = "15px";
+    connexion.insertBefore(errorMessage, connexion.lastElementChild);
   }
 });
 
-// Mettre à jour le lien de navigation lors du chargement de la page
 updateNavBasedOnAuthState();
